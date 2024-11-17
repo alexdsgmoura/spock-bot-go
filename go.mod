bd636ace1083ee32d30675bf31ee2a090cc88714
alexdsgmoura <alexdsgmoura@icloud.com>
2024-11-16 23:24:08 -0300

    /fig

--- go.mod
+++ go.mod
@@ -4,16 +4,33 @@ go 1.22.0
 
 toolchain go1.23.3
 
-require go.mau.fi/whatsmeow v0.0.0-20241114122515-3c0f25d54c22
+require (
+	go.mau.fi/whatsmeow v0.0.0-20241114122515-3c0f25d54c22
+	modernc.org/sqlite v1.33.1
+)
+
+require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
+	modernc.org/libc v1.55.3 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.8.0 // indirect
+	modernc.org/strutil v1.2.0 // indirect
+	modernc.org/token v1.1.0 // indirect
+)
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/chai2010/webp v1.1.1
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/mattn/go-sqlite3 v1.14.24
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/rs/zerolog v1.33.0 // indirect
+	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
 	go.mau.fi/libsignal v0.1.1 // indirect
 	go.mau.fi/util v0.8.2-0.20241113135441-636f8643f367 // indirect
 	golang.org/x/crypto v0.29.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"database/sql"
 	"fmt"
 	"math/rand/v2"
 	"os"
@@ -11,7 +13,6 @@ import (
 	"syscall"
 	"time"
 
-	_ "github.com/mattn/go-sqlite3"
 	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/proto/waE2E"
 	"go.mau.fi/whatsmeow/store/sqlstore"
@@ -19,13 +20,12 @@ import (
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
 	"google.golang.org/protobuf/proto"
+	_ "modernc.org/sqlite"
 )
 
 func generateQRCode(code string) error {
 	cmd := exec.Command("qrencode", "-t", "ansiutf8", code)
-
 	cmd.Stdout = os.Stdout
-
 	return cmd.Run()
 }
 
@@ -33,19 +33,48 @@ func getMessageText(message *waE2E.Message) string {
 	if message.GetConversation() != "" {
 		return message.GetConversation()
 	}
-
 	if message.ExtendedTextMessage.GetText() != "" {
 		return message.ExtendedTextMessage.GetText()
 	}
-
 	return ""
 }
 
 func formatSenderJID(jid types.JID) types.JID {
 	parts := strings.Split(jid.User, ":")
 	formattedUser := parts[0]
-
 	return types.NewJID(formattedUser, jid.Server)
+}
+
+func ConvertJPEGToWebP(input []byte, quality int) ([]byte, error) {
+	inputFile, err := os.CreateTemp("", "input-*.jpg")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(inputFile.Name())
+
+	_, err = inputFile.Write(input)
+	if err != nil {
+		return nil, err
+	}
+
+	err = inputFile.Sync()
+	if err != nil {
+		return nil, err
+	}
+
+	var outputBuffer bytes.Buffer
+	var errorBuffer bytes.Buffer
+
+	cmd := exec.Command("ffmpeg", "-i", inputFile.Name(), "-vf", "scale=512:512", "-q:v", fmt.Sprintf("%d", quality), "-f", "webp", "-")
+	cmd.Stdout = &outputBuffer
+	cmd.Stderr = &errorBuffer
+
+	err = cmd.Run()
+	if err != nil {
+		return nil, fmt.Errorf("ffmpeg error: %v, stderr: %s", err, errorBuffer.String())
+	}
+
+	return outputBuffer.Bytes(), nil
 }
 
 func eventHandler(client *whatsmeow.Client) func(evt interface{}) {
@@ -55,15 +84,13 @@ func eventHandler(client *whatsmeow.Client) func(evt interface{}) {
 			message := getMessageText(v.Message)
 			if message == "/ping" {
 				start := time.Now()
-
 				phoneNumber := formatSenderJID(v.Info.Sender)
-
 				messageID := v.Info.ID
 
 				err := client.MarkRead(
 					[]types.MessageID{messageID},
 					time.Now(),
-					phoneNumber,
+					v.Info.Chat,
 					phoneNumber,
 				)
 				if err != nil {
@@ -77,18 +104,18 @@ func eventHandler(client *whatsmeow.Client) func(evt interface{}) {
 					return
 				}
 
-				err = client.SendChatPresence(phoneNumber, "composing", "")
+				err = client.SendChatPresence(v.Info.Chat, "composing", "")
 				if err != nil {
 					fmt.Println(err)
 					return
 				}
 
-				durationSleep := time.Duration(rand.Float64()*(2.5-1)+1) * time.Second
+				durationSleep := time.Duration(rand.Float64()*(3000-1000-1)+1000) * time.Millisecond
 				time.Sleep(durationSleep)
 
 				duration := time.Since(start)
 
-				r, err := client.SendMessage(context.Background(), phoneNumber, &waE2E.Message{
+				r, err := client.SendMessage(context.Background(), v.Info.Chat, &waE2E.Message{
 					Conversation: proto.String(fmt.Sprintf("Pong! Tempo de resposta: %vms", duration.Abs().Milliseconds())),
 				})
 				if err != nil {
@@ -97,33 +124,180 @@ func eventHandler(client *whatsmeow.Client) func(evt interface{}) {
 					return
 				}
 
-				client.SendChatPresence(phoneNumber, "paused", "")
-
+				client.SendChatPresence(v.Info.Chat, "paused", "")
 				time.Sleep(1 * time.Second)
-
 				client.SendPresence("unavailable")
 
-				fmt.Println(phoneNumber)
+				fmt.Println("Mensagem enviada para:", phoneNumber.User)
+			}
 
-				return
+			if v.Info.IsGroup {
+				if message == "/fig" {
+					start := time.Now()
+
+					if v.Message.GetExtendedTextMessage() != nil && v.Message.GetExtendedTextMessage().ContextInfo.GetStanzaID() != "" {
+						if v.Message.GetExtendedTextMessage().GetContextInfo().GetQuotedMessage().GetImageMessage() != nil {
+							fmt.Println(*v.Message.GetExtendedTextMessage().GetContextInfo().GetQuotedMessage().GetImageMessage().Mimetype)
+
+							downloadedImage, err := client.Download(v.Message.GetExtendedTextMessage().GetContextInfo().GetQuotedMessage().GetImageMessage())
+							if err != nil {
+								fmt.Println(err)
+								return
+							}
+
+							webPImg, err := ConvertJPEGToWebP(downloadedImage, 100)
+							if err != nil {
+								fmt.Println(err)
+								return
+							}
+
+							resp, err := client.Upload(context.Background(), webPImg, whatsmeow.MediaImage)
+							if err != nil {
+								fmt.Println(err)
+								return
+							}
+
+							_, err = client.SendMessage(context.Background(), formatSenderJID(v.Info.Chat), &waE2E.Message{
+								StickerMessage: &waE2E.StickerMessage{
+									Mimetype:      proto.String("image/webp"),
+									URL:           &resp.URL,
+									MediaKey:      resp.MediaKey,
+									FileEncSHA256: resp.FileEncSHA256,
+									FileSHA256:    resp.FileSHA256,
+									FileLength:    &resp.FileLength,
+									Height:        proto.Uint32(80),
+									Width:         proto.Uint32(80),
+									PngThumbnail:  webPImg,
+									ContextInfo: &waE2E.ContextInfo{
+										StanzaID:      &v.Info.ID,
+										Participant:   proto.String(formatSenderJID(v.Info.Sender).String()),
+										QuotedMessage: v.Message,
+									},
+								},
+							})
+							if err != nil {
+								fmt.Printf("Erro ao enviar mensagem: %v\n", err)
+								return
+							}
+
+							return
+						}
+
+						err := client.SendPresence("available")
+						if err != nil {
+							fmt.Println(err)
+							return
+						}
+
+						err = client.MarkRead(
+							[]types.MessageID{v.Info.ID},
+							time.Now(),
+							v.Info.Chat,
+							formatSenderJID(v.Info.Sender),
+						)
+						if err != nil {
+							fmt.Println(err)
+							return
+						}
+
+						err = client.SendChatPresence(v.Info.Chat, "composing", "")
+						if err != nil {
+							fmt.Println(err)
+							return
+						}
+
+						durationSleep := time.Duration(rand.Float64()*(3000-1000-1)+1000) * time.Millisecond
+						time.Sleep(durationSleep)
+						duration := time.Since(start)
+
+						client.SendMessage(context.Background(), formatSenderJID(v.Info.Chat), &waE2E.Message{
+							ExtendedTextMessage: &waE2E.ExtendedTextMessage{
+								Text: proto.String(fmt.Sprintf("> A mensagem respondida não é uma imagem!\r\n\r\nTempo de execução: %vms", duration.Abs().Milliseconds())),
+								ContextInfo: &waE2E.ContextInfo{
+									StanzaID:      &v.Info.ID,
+									Participant:   proto.String(formatSenderJID(v.Info.Sender).String()),
+									QuotedMessage: v.Message,
+								},
+							},
+						})
+
+						client.SendChatPresence(v.Info.Chat, "paused", "")
+						time.Sleep(time.Duration(rand.Float64()*(3000-1000-1)+1000) * time.Millisecond)
+						client.SendPresence("unavailable")
+
+						return
+					} else {
+						err := client.SendPresence("available")
+						if err != nil {
+							fmt.Println(err)
+							return
+						}
+
+						err = client.MarkRead(
+							[]types.MessageID{v.Info.ID},
+							time.Now(),
+							v.Info.Chat,
+							formatSenderJID(v.Info.Sender),
+						)
+						if err != nil {
+							fmt.Println(err)
+							return
+						}
+
+						err = client.SendChatPresence(v.Info.Chat, "composing", "")
+						if err != nil {
+							fmt.Println(err)
+							return
+						}
+
+						durationSleep := time.Duration(rand.Float64()*(3000-1000-1)+1000) * time.Millisecond
+						time.Sleep(durationSleep)
+						duration := time.Since(start)
+
+						client.SendMessage(context.Background(), formatSenderJID(v.Info.Chat), &waE2E.Message{
+							ExtendedTextMessage: &waE2E.ExtendedTextMessage{
+								Text: proto.String(fmt.Sprintf("> É necessário responder a imagem que deseja transformar em figurinha!\r\n\r\nTempo de execução: %vms", duration.Abs().Milliseconds())),
+								ContextInfo: &waE2E.ContextInfo{
+									StanzaID:      &v.Info.ID,
+									Participant:   proto.String(formatSenderJID(v.Info.Sender).String()),
+									QuotedMessage: v.Message,
+								},
+							},
+						})
+
+						client.SendChatPresence(v.Info.Chat, "paused", "")
+						time.Sleep(time.Duration(rand.Float64()*(3000-1000-1)+1000) * time.Millisecond)
+						client.SendPresence("unavailable")
+
+						return
+					}
+				}
 			}
 		}
 	}
 }
 
 func main() {
-	dbLog := waLog.Stdout("Databse", "DEBUG", true)
-	container, err := sqlstore.New("sqlite3", "file:store.db?_foreign_keys=on", dbLog)
+	dbLog := waLog.Stdout("Database", "DEBUG", true)
+
+	sqlDB, err := sql.Open("sqlite", "file:store.db")
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("falha ao abrir o banco de dados: %w", err))
 	}
+	defer sqlDB.Close()
+
+	_, err = sqlDB.Exec("PRAGMA foreign_keys = ON")
+	if err != nil {
+		panic(fmt.Errorf("falha ao ativar foreign keys: %w", err))
+	}
+
+	container := sqlstore.NewWithDB(sqlDB, "sqlite", dbLog)
 
 	deviceStore, err := container.GetFirstDevice()
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("falha ao obter o dispositivo: %w", err))
 	}
 
-	//clientLog := waLog.Stdout("Client", "DEBUG", true)
 	noOpLogger := waLog.Noop
 	client := whatsmeow.NewClient(deviceStore, noOpLogger)
 	client.AddEventHandler(eventHandler(client))
